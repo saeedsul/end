a386b5d57656bb84425ab9b97364eae90e0988b7
Alex Wolf <alexrudiwolf@gmail.com>
2016-10-24 00:15:08 -0400

    end of lesson-04-06

--- Components/InventoryWidget.cs
+++ Components/InventoryWidget.cs
@@ -8,19 +8,19 @@ namespace OrderingApplication.Components
 {
     public class InventoryWidget : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(int threshold = 10)
         {
             var inventory = new List<InventoryItem>()
             {
                 new InventoryItem() {  Name = "Banner", Count = 8 },
                 new InventoryItem() {  Name = "Markers", Count = 3 },
                 new InventoryItem() {  Name = "Jacket", Count = 4 },
-                new InventoryItem() {  Name = "Hoodie", Count = 1 },
+                new InventoryItem() {  Name = "Hoodie", Count = 12 },
                 new InventoryItem() {  Name = "Poster", Count = 6 },
                 new InventoryItem() {  Name = "Pens", Count = 16 }
             };
 
-            return View(inventory);
+            return View(inventory.Where(x => x.Count <= threshold).ToList());
         }
     }
 
