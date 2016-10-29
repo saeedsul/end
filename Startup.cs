d601b879d7655bcfe2b8d65840ef0df4058dd6f9
Alex Wolf <alexrudiwolf@gmail.com>
2016-10-28 22:03:27 -0400

    end of lesson-05-06

--- Startup.cs
+++ Startup.cs
@@ -38,7 +38,7 @@ namespace OrderingApplication
             services.AddApplicationInsightsTelemetry(Configuration);
             services.AddScoped<IInventoryService, InventoryService>();
             services.AddSingleton<ISurveyService, SurveyService>();
-            services.AddSingleton<IFormDataService, JsonFormDataService>();
+            services.AddScoped<IFormDataService, JsonFormDataService>();
             services.AddMvc();
         }
 
