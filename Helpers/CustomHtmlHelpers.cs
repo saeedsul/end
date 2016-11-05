2e57df63047568983c3acb4f9d16307a566a004f
Alex Wolf <alexrudiwolf@gmail.com>
2016-11-05 16:16:29 -0400

    end of lesson-07-02

---
+++ Helpers/CustomHtmlHelpers.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace OrderingApplication.Helpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlContent LabeledTextboxFor<TModel, TResult>(
+            this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression, string labelText)
+        {
+            var label = htmlHelper.LabelFor(expression, labelText);
+            var textBox =  htmlHelper.TextBoxFor(expression, new { @class = "form-control" });
+
+            var writer = new StringWriter();
+            label.WriteTo(writer, HtmlEncoder.Default);
+            textBox.WriteTo(writer, HtmlEncoder.Default);
+
+            return new HtmlString(writer.GetStringBuilder().ToString());
+        }
+    }
+}
