acfebdd83bddb281835fe9ee030f54de87a6368b
Alex Wolf <alexrudiwolf@gmail.com>
2016-11-04 23:35:26 -0400

    model adjustments

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,21 @@ namespace OrderingApplication.Models
 {
     public class Order
     {
+        [Required]
         public string ClientId { get; set; }
+        [Required]
         public string CompanyName { get; set; }
+        [Required]
         public string OrderDate { get; set; }
+        [Required]
         public string Stage { get; set; }
         public List<OrderProductVM> Products { get; set; }
+        [Required]
         public string ShippingAddress { get; set; }
+        [Required]
         public string OrderInstructions { get; set; }
+        [Required]
+        [MaxLength(25)]
         public string Notes { get; set; }
     }
 
